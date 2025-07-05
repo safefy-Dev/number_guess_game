adfc36e3b0d4be6107cea9c7df1072709b9e3597
safe-Dev <DataAnalystSHD@gmail.com>
2025-07-06 02:06:30 +0700

    gogo

--- supabase_client.py
+++ supabase_client.py
@@ -1,7 +1,10 @@
-
 from supabase import create_client
 import os
+from dotenv import load_dotenv
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
-supabase = create_client(url, key)
+load_dotenv()
+
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(supabase_url, supabase_key)
